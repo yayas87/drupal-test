693bdfe7507bcba8fa514263d87e5d55a3e3f372
Jaseer K <mail@jaseer.in>
2024-06-14 18:53:00 +0530

    update

--- web/modules/custom/ajman_core/js/ajman_core.js
+++ web/modules/custom/ajman_core/js/ajman_core.js
@@ -1,24 +1,39 @@
-(function (Drupal, $) {
-    Drupal.behaviors.ajmanCore = {
-      attach: function (context, settings) {
-        var $serviceAdaptableField = $('select[name="field_service_adaptable[0][value]"]', context);
-        var $elloborateFieldWrapper = $('.field-elloborate-wrapper', context);
-  
-        // Function to toggle the visibility of the elloborate field.
-        function toggleElloborateField() {
-          if ($serviceAdaptableField.val() === 'yes') {
-            $elloborateFieldWrapper.show();
-          } else {
-            $elloborateFieldWrapper.hide();
-          }
+(function ($, Drupal) {
+  Drupal.behaviors.ajmanCoreBehavior = {
+    attach: function (context, settings) {
+      // Function to show/hide field_elloborate based on the selected value and set it as required.
+      function toggleFieldElloborate(serviceWrapper) {
+        var selectedValue = serviceWrapper.find('select[name$="[field_service_adaptable]"]').val();
+        var fieldElloborate = serviceWrapper.find('.field--name-field-elloborate');
+        var fieldElloborateInput = fieldElloborate.find('input, textarea');
+
+        if (selectedValue === 'yes') {
+          fieldElloborate.show();
+          fieldElloborateInput.prop('required', true);
+        } else {
+          fieldElloborate.hide();
+          fieldElloborateInput.prop('required', false);
         }
-  
-        // Initialize the visibility on page load.
-        toggleElloborateField();
-  
-        // Add event listener to toggle the field on change.
-        $serviceAdaptableField.on('change', toggleElloborateField);
       }
-    };
-  })(Drupal, jQuery);
-  
+
+      // Iterate over each service paragraph.
+      $('.paragraph-type--service', context).each(function () {
+        var serviceWrapper = $(this);
+
+        // Initially hide the field_elloborate field and remove required attribute.
+        var fieldElloborate = serviceWrapper.find('.field--name-field-elloborate');
+        var fieldElloborateInput = fieldElloborate.find('input, textarea');
+        fieldElloborate.hide();
+        fieldElloborateInput.prop('required', false);
+
+        // Bind the change event to the field_service_adaptable select element.
+        serviceWrapper.find('select[name$="[field_service_adaptable]"]').change(function () {
+          toggleFieldElloborate(serviceWrapper);
+        });
+
+        // Initial call to set the correct visibility and required attribute on page load.
+        toggleFieldElloborate(serviceWrapper);
+      });
+    }
+  };
+})(jQuery, Drupal);
